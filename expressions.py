a788c66cf6f52ef18b0724db081b90b2d14edf1f
Andy Zhou <wz337@cam.ac.uk>
2022-07-16 11:40:47 +0100

    crossover operations using list representation

---
+++ expressions.py
@@ -0,0 +1,69 @@
+from sympy import *
+from random import randint, choices
+
+
+# class Node:
+#     def __init__(self, type, children=None):
+#         self.type = type
+#         self.children = children or []
+#
+#     def to_expression(self):
+#         raise NotImplementedError
+#
+# class Addition(Node):
+#     def to_expression(self):
+#         return sum(child.to_expression() for child in self.children)
+#
+# class Multiplication(Node):
+#     def to_expression(self):
+#         return prod(child.to_expression() for child in self.children)
+#
+# class TreeExpression:
+#     ...
+
+# example expression: x, ('+', ('*', 'x', 'y'), ('*', 'x', 'z')): x * y + x * z
+def count_nodes(expr):
+    if isinstance(expr, tuple):
+        return 1 + sum(count_nodes(child) for child in expr)
+    else:
+        return 1
+
+
+def get_random_subtree(expr):
+    if isinstance(expr, tuple):
+        weights = [count_nodes(child) for child in expr[1:]]
+        if randint(0, sum(weights)) == 0:
+            return expr
+        else:
+            return get_random_subtree(choices(expr[1:], weights=weights, k=1)[0])
+    else:
+        return expr
+
+
+print(get_random_subtree(('+', ('*', 'x', 'y'), ('*', 'x', 'z'))))
+
+
+# ('+', ('*', 'x', 'y'), ('*', 'x', 'z'))
+
+def random_sub(expr, replacement):
+    if isinstance(expr, tuple):
+        weights = [count_nodes(child) for child in expr[1:]]
+        if randint(0, sum(weights)) == 0:
+            return replacement
+        else:
+            replace_node_num = choices(range(1, len(expr)), weights=weights, k=1)[0]
+            return expr[:replace_node_num] + (random_sub(expr[replace_node_num], replacement),) + expr[
+                                                                                                  replace_node_num + 1:]
+    else:
+        return replacement
+
+
+def cross_over(expr1, expr2):
+    if randint(0, 1) == 0:
+        return random_sub(expr1, get_random_subtree(expr2))
+    else:
+        return random_sub(expr2, get_random_subtree(expr1))
+
+
+# for i in range(100):
+#     print(cross_over(('+', ('*', 'x', 'y'), ('*', 'x', 'z')), ('symb', 't', 'a', 'b')))
